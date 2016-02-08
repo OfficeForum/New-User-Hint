576755ada973f044deeed163e93cd9cba99a9d5b
Karsten Ude <oxpus.mail@googlemail.com>
2016-02-08 20:43:54 +0100

    Final Release 1.0.1
    FIX Rewrite URL without app.php does not work properly

--- newuserhint/migrations/release_1_0_1.php
+++ newuserhint/migrations/release_1_0_1.php
@@ -10,13 +10,18 @@
 
 namespace oxpus\newuserhint\migrations;
 
-class release_1_0_0 extends \phpbb\db\migration\migration
+class release_1_0_1 extends \phpbb\db\migration\migration
 {
-	var $ext_version = '1.0.0';
+	var $ext_version = '1.0.1';
 
 	public function effectively_installed()
 	{
 		return isset($this->config['newuserhint_version']) && version_compare($this->config['newuserhint_version'], $this->ext_version, '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array('\oxpus\newuserhint\migrations\release_1_0_0');
 	}
 
 	public function update_data()
